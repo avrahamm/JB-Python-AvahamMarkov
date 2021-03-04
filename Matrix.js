75cf2c5fb0d1809a305913fe3f91d6afd14cdc15
avrahamm <papaabramg@gmail.com>
2021-03-04 19:35:11 +0200

    fixed solution

--- Matrix.js
+++ Matrix.js
@@ -4,7 +4,11 @@ function compareNumbers(a, b) {
     return a - b;
 }
 
-module.exports = function(arr) {
+/**
+ *
+ * @param arr 2 dimensional array assumed
+ */
+module.exports = function(arr = [[]]) {
     this.inputArr = arr;
     this.cellsArr = [];
     this.riverLengths = [];
@@ -23,6 +27,7 @@ module.exports = function(arr) {
 
     /**
      * iterate over all cells to find river lengths
+     * start in each cell.
      */
     this.calculateRiverLengths = function () {
         for( let row = 0; row < this.cellsArr.length; row++) {
@@ -39,23 +44,35 @@ module.exports = function(arr) {
     }
 
     /**
-     * called for cell with value 1 only
+     * Top and left were already checked.
+     * Counts current cell, right and bottom rivers recursively,
+     * and returns the result.
      * @param startRow
      * @param startCol
      * @returns {number}
      */
     this.calculateRiverLength = function (startRow, startCol) {
+        let curCell = this.cellsArr[startRow][startCol];
+        curCell.taken = true;
+        // should be called for cell with value 1 only, yet checking
+        if (curCell.value === 0) {
+            return 0;
+        }
         let curRow = this.cellsArr[startRow];
-        let curCell = this.cellsArr[startRow, startCol];
-        // curCell.taken = true; // fails here
         let curRiverLength = 1;
         let rightLength = 0;
-        if ((startCol + 1) < curRow.length ) {
+        if ((startCol + 1) < curRow.length
+            && ( this.cellsArr[startRow][startCol+1].value === 1)
+            && ( this.cellsArr[startRow][startCol+1].taken === false)
+        ) {
             rightLength = this.calculateRiverLength(startRow, startCol + 1)
         }
 
         let bottomLength = 0;
-        if ((startRow + 1) < this.cellsArr.length ) {
+        if ((startRow + 1) < this.cellsArr.length
+            && ( this.cellsArr[startRow + 1][startCol].value === 1)
+            && ( this.cellsArr[startRow + 1][startCol].taken === false)
+        ) {
             bottomLength = this.calculateRiverLength(startRow + 1, startCol)
         }
         curRiverLength = curRiverLength + rightLength + bottomLength;
@@ -64,7 +81,6 @@ module.exports = function(arr) {
 
     this.getRiverLengths = function () {
         this.calculateRiverLengths();
-        console.log(this.cellsArr)
         return this.riverLengths.sort(compareNumbers);
     }
 }
--- test.js
+++ test.js
@@ -1,14 +1,34 @@
 const Matrix = require('./Matrix');
 
-const arr1 = [[1,0]];
+const arr1 = [
+    [1,0,0,1],
+    [0,0,1,0],
+    [1,0,1,0],
+    [1,0,1,0],
+];
+
+const arr2 = [
+    [0,0,0,1,0,0,0,1],
+    [1,1,0,0,0,0,1,0],
+    [0,0,0,0,1,1,0,1],
+    [1,1,1,0,0,0,0,0],
+];
+
+const arr3 = [
+    [0,0,0,0,1],
+    [0,1,1,1,0],
+    [0,1,0,1,0],
+    [0,1,1,1,0],
+    [1,0,0,0,0],
+];
 
 function magic(arr) {
     const matrix =  new Matrix(arr);
     matrix.buildCellMatrix();
-    const result = matrix.getRiverLengths();
-    // result = [1,1,2];
-    return result;
+    return matrix.getRiverLengths();
 }
 
 console.log(magic(arr1));
+console.log(magic(arr2));
+console.log(magic(arr3));
 
